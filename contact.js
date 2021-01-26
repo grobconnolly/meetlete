ec16185e47acc38f8e428663a6a4249c1311cb31
rob connolly <rob@textjet.com>
2021-01-26 13:28:41 -0800

    form redirect

--- contact.js
+++ contact.js
@@ -9,7 +9,7 @@ if (fanForm) {
     const formData = new FormData(fanForm);
     formData.append("type", "fan");
     fetch(scriptURL, { method: "POST", body: formData })
-      .then((response) => console.log("Success!", response))
+      .then((response) => window.location.href = "index.html")
       .catch((error) => console.error("Error!", error.message));
   });
 }
@@ -21,7 +21,7 @@ if (contestForm) {
     const formData = new FormData(contestForm);
     formData.append("type", "contest");
     fetch(scriptURL, { method: "POST", body: formData })
-      .then((response) => console.log("Success!", response))
+    .then((response) => window.location.href = "index.html")
       .catch((error) => console.error("Error!", error.message));
   });
 }
@@ -34,7 +34,7 @@ if (talentForm) {
     const formData = new FormData(talentForm);
     formData.append("type", "talent");
     fetch(scriptURL, { method: "POST", body: formData })
-      .then((response) => console.log("Success!", response))
+    .then((response) => window.location.href = "index.html")
       .catch((error) => console.error("Error!", error.message));
   });
 }
